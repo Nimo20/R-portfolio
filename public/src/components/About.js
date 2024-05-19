63e3164a1460faedb2809b1ba24a36ae107588ab
Nimo <shirille21@gmail.com>
2024-05-19 05:47:01 -0500

    .

--- public/src/components/About.js
+++ public/src/components/About.js
@@ -2,16 +2,17 @@ import React from 'react';
 
 const About = () => {
     return (
-        <section>
-            <h2>About Me</h2>
-            <img src="public/image/profile.jpg" alt="Developer" />
-            <p><br>Am Nimo Shirille</br>
-                Am on a journey to become a skilled full-stack developer. With a background in problem solving and Yard coordinator at a food distrubution center, currently am dedicated to mastering both the frontend and backend aspects of web development.
+        <div className="about-container">
+            <h1>About Me</h1>
+            <img src="public/public/image/Profile.JPG" alt="Developer" className="profile-image" />
+            <p>
+                Am on a journey to become a skilled full-stack developer. With a background in problem solving and Yard coordinator at a food distribution center, currently am dedicated to mastering both the frontend and backend aspects of web development.
 
-                I thrives on challenges and enjoys diving into new technologies and frameworks to expand their skill set. They are currently focusing on learning languages such as JavaScript and Python, as well as popular frameworks like React and Node.js.
+                I thrive on challenges and enjoy diving into new technologies and frameworks to expand my skill set. I am currently focusing on learning languages such as JavaScript and Python, as well as popular frameworks like React and Node.js.
 
-                Outside of coding, I enjoys spending time with my family. Am are excited to continue my journey in the world of full-stack development and am eager to contribute to innovative projects that make a positive impact.</p>
-        </section>
+                Outside of coding, I enjoy spending time with my family. I am excited to continue my journey in the world of full-stack development and am eager to contribute to innovative projects that make a positive impact.
+            </p>
+        </div>
     );
 };
 
